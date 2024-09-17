d61a2c560ff50a6f3c3a3e0a1001f4be5d3a700a
Ankit Bhawnani <bhawnaniankit@gmail.com>
2024-09-18 00:34:50 +0530

    google auth init

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const Appbar = () => {
+  const session = useSession();
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>
+          MusicMap
+        </div>
+        <div>
+          {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => { signOut() }}>SignOut</button>}
+          {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => { signIn() }}>SignIn</button>}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Appbar
